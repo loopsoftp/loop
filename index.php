12f80c3e26ee54f2eefbbaa894c80f5109e25827
loopsoftp <3476146@qq.com>
2017-10-26 13:36:55 +0800

    666

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LoopSoft
+ * Date: 2017-10-25
+ * Time: 16:59
+ */
+echo 'fuck you';
+echo '<head>
+<title>what the fuck?</title>
+</head>
+<h1 style="color:blue">This is headline 1</h1>
+
+';
+
+
+
